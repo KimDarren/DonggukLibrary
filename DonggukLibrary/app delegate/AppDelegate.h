0684a3b2f12ff5322345d4a6117d37f822d0cf40
KimDarren <korean.darren@gmail.com>
2015-05-07 04:04:49 +0900

    Initialize xcode project

---
+++ DonggukLibrary/app delegate/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DonggukLibrary
+//
+//  Created by 1001246 on 2015. 5. 7..
+//  Copyright (c) 2015년 USAIU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Podfile
@@ -0,0 +1,5 @@
+platform :ios, '8.0'
+xcodeproj 'DonggukLibrary.xcodeproj'
+
+pod 'Masonry', '0.6.1'
+pod 'AFNetworking', '2.5.3'
